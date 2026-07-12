59b42d4c9b1792887b729b17496e1b58a3b62847
HARSH-HEMANI <hhemani365@gmail.com>
2026-07-12 16:56:16 +0530

    feat: update user model schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -48,7 +48,7 @@ const UserSchema = new mongoose.Schema({
 // Encrypt password before saving
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
